dc84247732bf566f489618c572513e69730f7c01
josephoneill <oneill.292@osu.edu>
2019-05-07 20:48:36 -0400

    fix Chip.php formatting and indentation

--- src/Chip.php
+++ src/Chip.php
@@ -1,38 +1,48 @@
 <?php
-    class Chip {
-		private $firstColor;
-		private $secondColor;
-		private $used;
 
-   		function __construct($firstColor, $secondColor) {
-			$this->firstColor = $firstColor;
-			$this->secondColor = $secondColor;
-			$this->used = false;
-		}
+class Chip
+{
+    private $firstColor;
+    private $secondColor;
+    private $used;
 
-        public function setFirstColor($firstColor) { 
-			$this->firstColor = $firstColor;  
- 		}
- 
-   		public function getFirstColor() {
-			return $this->firstColor;
-        }
-        
-        public function setSecondColor($secondColor) { 
-			$this->firstColor = $secondColor;  
- 		}
- 
-   		public function getSecondColor() {
-			return $this->secondColor;
-		}
+    function __construct($firstColor, $secondColor)
+    {
+        $this->firstColor = $firstColor;
+        $this->secondColor = $secondColor;
+        $this->used = false;
+    }
 
-		public function setUsed($used) {
-			$this->used = $used;
-		}
+    public function setFirstColor($firstColor)
+    {
+        $this->firstColor = $firstColor;
+    }
 
-		public function isUsed() {
-			return $this->used;
-		}
-		
-	}
+    public function getFirstColor()
+    {
+        return $this->firstColor;
+    }
+
+    public function setSecondColor($secondColor)
+    {
+        $this->firstColor = $secondColor;
+    }
+
+    public function getSecondColor()
+    {
+        return $this->secondColor;
+    }
+
+    public function setUsed($used)
+    {
+        $this->used = $used;
+    }
+
+    public function isUsed()
+    {
+        return $this->used;
+    }
+
+}
+
 ?>
